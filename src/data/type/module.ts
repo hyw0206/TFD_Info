f933a8b840302dabfa50cb5f1a7bedde58ddcd62
hyw0206 <114200503+hyw0206@users.noreply.github.com>
2024-08-01 18:00:07 +0900

    Update module.ts

--- src/data/type/module.ts
+++ src/data/type/module.ts
@@ -2,7 +2,7 @@ export interface Module {
   module_name: string;
   module_id: string;
   image_url: string;
-  module_type?: string;
+  module_type: string | null;
   module_tier: string;
   module_socket_type: string;
   module_class: string;
